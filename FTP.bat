0bf90bfc1b34b8355b63900db3fc65554142e69a
VitorFake <vitorfake2017@gmail.com>
2017-02-18 23:48:08 -0300

    Create FTP.bat

---
+++ FTP.bat
@@ -0,0 +1,9 @@
+ftp -i -s:NomeDoSeuArquivo.bat
+open ftp.seuSite.com.br
+SeuUsuario
+SuaSenha
+put c:\\SeuArquivo.txt                             
+cd Pasta
+binary
+get Arquivo.EXE c:\\Arquivo.EXE
+quit
